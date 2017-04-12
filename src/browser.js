bc3333b0fe039dcedcef8acaca3da5f766f37c00
Jeffrey Burt <jeffrey.burt@oicr.on.ca>
2017-04-12 11:52:40 -0400

    init

---
+++ src/browser.js
@@ -0,0 +1,1 @@
+window.composeNS = require('./index.js').default;
---
+++ src/index.js
@@ -0,0 +1,10 @@
+export default function composeNS(ns, ...funcs) {
+  if (funcs.length === 0) return arg => arg;
+
+  return [
+    Wrapped => (props) => Wrapped({ parentProps: props }),
+    ...funcs,
+    Wrapped => ({ parentProps, ...props }) => Wrapped({ ...parentProps, [ns]: props }),
+  ] 
+    .reduce((a, b) => (...args) => a(b(...args)));
+};
