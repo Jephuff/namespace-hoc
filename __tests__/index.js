9e1d2a3586e35e955d5159afe6947cf5aee355f6
Jeffrey Burt <jeffrey.m.burt@gmail.com>
2017-04-19 07:13:17 -0400

    map parent props instead of passing them all in

--- __tests__/index.js
+++ __tests__/index.js
@@ -55,3 +55,91 @@ test('composeNS props are available', () => {
   expect(component.firstCall.args[0].namespace.state.namespace).toBe('namespace')
   expect(component.secondCall.args[0].namespace.state.namespace).toBe('namespace')
 })
+
+test('composeNS parentMap passed in specified props', () => {
+  const component = sinon.spy(() => null)
+  component.displayName = 'component'
+
+  const StateContainer = compose(
+    withState('state', 'setState', { num: 5 }),
+    composeNS(
+      'namespace',
+      ['state'],
+      mapProps(props => ({
+        ...props,
+        num: props.state.num * 10,
+      }))
+    ),
+    mapProps(({ state, ...rest }) => ({
+      ...rest,
+      state,
+      val: state.val,
+    }))
+  )(component)
+
+  mount(<StateContainer />)
+  const { setState } = component.firstCall.args[0]
+  setState(state => ({ ...state, num: 2 }));
+
+  expect(component.firstCall.args[0].namespace.num).toBe(50)
+  expect(component.secondCall.args[0].namespace.num).toBe(20)
+})
+
+test('composeNS parentMap does not pass in unspecified props', () => {
+  const component = sinon.spy(() => null)
+  component.displayName = 'component'
+
+  const StateContainer = compose(
+    withState('state', 'setState', { num: 5 }),
+    composeNS(
+      'namespace',
+      mapProps(({ state = {}, ...props }) => ({
+        ...props,
+        num: state.num,
+      }))
+    ),
+    mapProps(({ state, ...rest }) => ({
+      ...rest,
+      state,
+      val: state.val,
+    }))
+  )(component)
+
+  mount(<StateContainer />)
+  const { setState } = component.firstCall.args[0]
+  setState(state => ({ ...state, num: 2 }));
+
+  expect(component.firstCall.args[0].namespace.num).toBe(undefined)
+  expect(component.secondCall.args[0].namespace.num).toBe(undefined)
+})
+
+test('composeNS parentMap uses aliases', () => {
+  const component = sinon.spy(() => null)
+  component.displayName = 'component'
+
+  const StateContainer = compose(
+    withState('state', 'setState', { num: 5 }),
+    composeNS(
+      'namespace',
+      {
+        state: 'parentState',
+      },
+      mapProps(({ parentState = {}, ...props }) => ({
+        ...props,
+        num: parentState.num,
+      }))
+    ),
+    mapProps(({ state, ...rest }) => ({
+      ...rest,
+      state,
+      val: state.val,
+    }))
+  )(component)
+
+  mount(<StateContainer />)
+  const { setState } = component.firstCall.args[0]
+  setState(state => ({ ...state, num: 2 }));
+
+  expect(component.firstCall.args[0].namespace.num).toBe(5)
+  expect(component.secondCall.args[0].namespace.num).toBe(2)
+})
--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
 import React from 'react'
 
-export default function composeNS(ns, ...funcs) {
+const parentKey = '__parent_props__'; // If React adds support for Symbol props, this can be switched to use them.
+
+function getParentMap(mapOpt) {
+  if (Array.isArray(mapOpt)) {
+    return mapOpt.map((key) => [key, key])
+  } else if(mapOpt && typeof mapOpt === 'object') {
+    return Object.entries(mapOpt);
+  }
+
+  return [];
+}
+
+export default function composeNS(ns, ...args) {
+  const hasParentMap = Array.isArray(args[0]) || typeof args[0] === 'object';
+  const parentMap = getParentMap(hasParentMap && args[0]);
+  const funcs = hasParentMap ? args.slice(1) : args;
+
   if (funcs.length === 0) return arg => arg;
 
   return [
-    Wrapped => (props) => <Wrapped parentProps={props} />,
+    Wrapped => (props) => (
+      <Wrapped
+        {...{
+          [parentKey]: props,
+          ...(
+            parentMap
+              .reduce((acc, [key, alias]) => ({
+                ...acc,
+                [alias]: props[key],
+              }), {})
+          ),
+        }}
+      />
+    ),
     ...funcs,
-    Wrapped => ({ parentProps, ...props }) => <Wrapped {...{ ...parentProps, [ns]: props }} />,
+    Wrapped => ({ [parentKey]: parentProps, ...props }) => <Wrapped {...{ ...parentProps, [ns]: props }} />,
   ] 
     .reduce((a, b) => (...args) => a(b(...args)));
 };
